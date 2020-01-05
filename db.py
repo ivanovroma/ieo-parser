1ce6022454dcb8bdaf562d1c1f7b5eb84c26df17
roma <romaivanov@protonmail.com>
2020-01-05 05:48:13 +0200

    Полный рефакторинг

--- db.py
+++ db.py
@@ -1,19 +1,48 @@
 import json
 
-def get_list():
-    try:
-        with open('db.json') as file:
-            data = json.load(file)
-        return data
-    except:
-        return []
-    
+class DB:
+    def __write(self, new_list):
+        with open('db.json', 'w') as file:
+            json.dump(new_list, file, indent=2)
 
-def write_one(ieo):
-    ieos = get_list()
-    ieos.insert(0, ieo)
+    def get_not_success(self):
+        saved_list = self.get_list()
+        not_success_list = []
 
-    with open('db.json', 'w') as file:
-        json.dump(ieos, file, indent=2)
-        name = ieo['name']
-        print(f'Добавил в db - {name} \n')
+        for ieo in saved_list:
+            success = ieo['success_link_downloaded']
+
+            if not success:
+                not_success_list.append(ieo)
+
+        return not_success_list
+
+    def get_list(self):
+        try:
+            with open('db.json') as file:
+                data = json.load(file)
+            return data
+        except:
+            return []
+        
+
+    def append_list(self, append_ieo_list):
+        saved_list = self.get_list()
+        new_ieo_list = append_ieo_list + saved_list
+
+        self.__write(new_ieo_list)
+
+    def update_every(self, update_list):
+        saved_list = self.get_list()
+
+        for update_ieo in update_list:
+            update_ieo_href = update_ieo['href']
+
+            for saved_ieo in saved_list:
+                saved_ieo_href = saved_ieo['href']
+
+                if update_ieo_href == saved_ieo_href:
+                    saved_ieo['success_link_downloaded'] = True
+                    saved_ieo['links'] = update_ieo['links']
+
+        self.__write(saved_list)
--- parser.py
+++ parser.py
@@ -1,131 +1,195 @@
 from bs4 import BeautifulSoup
 import requests
+from Logger import Logger
 
-def get_html(route):
-    base_url = 'https://icobench.com'
-    url = base_url + route
+logger = Logger()
 
-    try:
-        response = {
+class Parser:
+    __base_url = 'https://icobench.com'
+    __recheck_list = []
+
+    def __get_html(self, route):
+        url = self.__base_url + route
+
+        try:
+            response = {
+                'success': True,
+                'body': requests.get(url)
+            }
+        except:
+            response = {
+                'success': False,
+                'message': 'get_html_fail',
+                'subject': url
+            }
+
+        success = response['success']
+        if not success:
+            return response
+
+        body = response['body']
+        soup = BeautifulSoup(body.text, 'html.parser')
+
+        return {
             'success': True,
-            'body': requests.get(url)
-        }
-    except:
-        response = {
-            'success': False,
-            'message': 'get_html_fail',
-            'subject': url
+            'soup': soup
         }
 
-    success = response['success']
-    if not success:
-        print(f'Не удалось загрузить страницу - {url}')
-        return response
+    def get_list(self):
+        response = self.__get_html('/ieo')
 
-    body = response['body']
-    soup = BeautifulSoup(body.text, 'html.parser')
+        success = response['success']
+        if not success:
+            return response
 
-    return {
-        'success': True,
-        'soup': soup
-    }
+        soup = response['soup']
 
-def get_list():
-    print('Загружаю список IEO')
-    response = get_html('/ieo')
+        ieocols = soup.find(id='ieocols')
+        if not ieocols:
+            return {
+                'success': False,
+                'message': 'ieocols_not_found',
+                'subject': '/ieo'
+            }
+        
+        table = ieocols.find('table')
 
-    success = response['success']
-    if not success:
-        print('Не удалось получить страницу с списком')
-        return response
+        if not table:
+            return {
+                'success': False,
+                'message': 'table_not_found',
+                'subject': '/ieo'
+            }
 
-    soup = response['soup']
+        trs = table.find_all('tr')
+        valid_trs = len(trs) > 0
+        if not valid_trs:
+            return {
+                'success': False,
+                'message': 'rows_not_found',
+                'subject': '/ieo'
+            }
 
-    ieocols = soup.find(id='ieocols')
-    if not ieocols:
-        print('#ieocols не найден')
+        parsed_list = []
+        for tr in trs:
+            ico_data = tr.find('td', class_='ico_data')
+
+            if not ico_data:
+                continue
+
+            a = ico_data.find('a', class_='name')
+
+            parsed_list.append({
+                'name': a.get_text(strip=True),
+                'href': a.get('href'),
+                'url': self.__base_url + a.get('href'),
+                'success_link_downloaded': False,
+                'links': []
+            })
+
         return {
-            'success': False,
-            'message': 'ieocols_not_found',
+            'success': True,
+            'list': parsed_list
         }
+
+    def __get_one(self, ieo):
+        name_ieo = ieo['name']
+        href_ieo = ieo['href']
+        
+        response = self.__get_html(href_ieo)
+
+        success = response['success']
+        if not success:
+            return response
+
+        soup = response['soup']
+        socials = soup.find('div', class_='socials')
+        if not socials:
+            return {
+                'success': False,
+                'message': 'socials_not_found',
+                'subject': href_ieo
+            }
+
+        a_list = socials.find_all('a')
+
+        links = ieo['links']
+        for a in a_list:
+            links.append({
+                'name': a.get_text(strip=True),
+                'url': a['href']
+            })
+
+        ieo['links'] = links
+
+        return {
+            'success': True,
+            'ieo': ieo
+        }
+
+    def get_every(self, new_ieo_list):
+        error = {
+            'success': False,
+            'message': '',
+            'subject': ''
+        }
+
+        parsed_list = []
+
+        # Для каждого нового IEO парсим социалки
+        for new_ieo in new_ieo_list:
+            parsed = self.__get_one(new_ieo)
+
+            success = parsed['success']
+            if not success:
+                # Если не удалось загрузить страницу, повторим попытку позже
+                if (parsed.get('message') == 'get_html_fail'):
+                    error['message'] = parsed.get('message')
+                    error['subject'] = parsed.get('subject')
+                    break
+
+                logger.warning('Не удалось получить данные об IEO', parsed)
+
+                parsed_list.append(new_ieo)
+                continue
+
+
+            parsed_ieo = parsed['ieo']
+
+            name = parsed_ieo['name']
+            logger.info(f'Полученны данные по проекту {name}')
+            
+            parsed_ieo['success_link_downloaded'] = True
+
+            # Все ок, пишем в db
+            parsed_list.append(parsed_ieo)
+        else:
+            return {
+                'success': True,
+                'list': parsed_list
+            }
+
+        return error
     
-    table = ieocols.find('table')
+    def recheck(self, not_success_list):
+        result = []
 
-    if not table:
-        print('На полученной странице таблица не найдена')
+        recheck = self.get_every(not_success_list)
+
+        success = recheck['success']
+        if not success:
+            return recheck
+
+        recheck_list = recheck['list']
+
+        for ieo in recheck_list:
+            success = ieo['success_link_downloaded']
+            if not success:
+                continue
+            
+            result.append(ieo)
+
         return {
-            'success': False,
-            'message': 'table_not_found',
+            'success': True,
+            'list': result
         }
-
-    trs = table.find_all('tr')
-    valid_trs = len(trs) > 0
-    if not valid_trs:
-        print('В полученной таблице строк не найдено')
-        return {
-            'success': False,
-            'message': 'rows_not_found'
-        }
-
-    parsed_list = []
-    for tr in trs:
-        ico_data = tr.find('td', class_='ico_data')
-
-        if not ico_data:
-            continue
-
-        a = ico_data.find('a', class_='name')
-
-        parsed_list.append({
-            'name': a.get_text(strip=True),
-            'href': a.get('href'),
-            'links': [
-                {
-                    'name': 'ICOBench',
-                    'url': 'https://icobench.com' + a.get('href')
-                }
-            ]
-        })
-
-    return {
-        'success': True,
-        'list': parsed_list
-    }
-
-def get_one(ieo):
-    name_ieo = ieo['name']
-    href_ieo = ieo['href']
-    print(f'Загружаю данные по проекту {name_ieo}')
-    
-    response = get_html(href_ieo)
-
-    success = response['success']
-    if not success:
-        print(f'Не удалось получить страницу с проектом {name_ieo}')
-        return response
-
-    soup = response['soup']
-    socials = soup.find('div', class_='socials')
-    if not socials:
-        print('Не найдена информация о социальных сетях на странице')
-        return {
-            'success': False,
-            'message': 'socials_not_found',
-            'subject': href_ieo
-        }
-
-    a_list = socials.find_all('a')
-
-    links = ieo['links']
-    for a in a_list:
-        links.append({
-            'name': a.get_text(strip=True),
-            'url': a['href']
-        })
-
-    ieo['links'] = links
-
-    return {
-        'success': True,
-        'ieo': ieo
-    }
